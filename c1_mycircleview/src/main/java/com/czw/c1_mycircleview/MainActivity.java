2bcf910d4d1abb7053a104af83ebc7a0ddc4a3e0
wenSome <985051537@qq.com>
2018-07-17 09:38:56 +0800

    新建类czw789

--- c1_mycircleview/src/main/java/com/czw/c1_mycircleview/MainActivity.java
+++ c1_mycircleview/src/main/java/com/czw/c1_mycircleview/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //新分支123
+        //czw456
     }
 }
