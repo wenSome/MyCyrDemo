ffd7c3a1d00073769226d78d8c02ab3c77e26ef4
wenSome <985051537@qq.com>
2018-07-17 10:07:08 +0800

    修改text类,添加内容数据

--- c1_mycircleview/src/main/java/com/czw/c1_mycircleview/text.java
+++ c1_mycircleview/src/main/java/com/czw/c1_mycircleview/text.java
@@ -11,4 +11,14 @@ package com.czw.c1_mycircleview;
  */
 
 public class text {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
