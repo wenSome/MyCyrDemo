1c55d659091738c0fde095794d70880e1015e679
wenSome <985051537@qq.com>
2018-07-17 09:30:26 +0800

    新建类123

--- c1_mycircleview/src/main/java/com/czw/c1_mycircleview/MainActivity.java
+++ c1_mycircleview/src/main/java/com/czw/c1_mycircleview/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //新分支
+        //新分支123
     }
 }
