1a758688b2dd09500697f7426bb790cb816a0dd2
wenSome <985051537@qq.com>
2018-07-17 10:04:48 +0800

    修改text类

--- c1_mycircleview/src/main/java/com/czw/c1_mycircleview/text.java
+++ c1_mycircleview/src/main/java/com/czw/c1_mycircleview/text.java
@@ -11,4 +11,14 @@ package com.czw.c1_mycircleview;
  */
 
 public class text {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
